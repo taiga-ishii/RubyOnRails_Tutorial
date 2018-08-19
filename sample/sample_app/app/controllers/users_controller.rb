d34ba57016ce366750cfee6702346f87a5fe7bee
taiga-ishii <tiger1410111@gmail.com>
2018-08-19 13:19:49 +0900

    friendly-fowarding

--- sample/sample_app/app/controllers/users_controller.rb
+++ sample/sample_app/app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+   before_action :logged_in_user, only: [:edit, :update]
+   before_action :correct_user,   only: [:edit, :update]
   def new
     @user = User.new
   end
@@ -23,6 +25,17 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      # 更新に成功した場合を扱う。
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
   private
 
    def user_params
@@ -30,4 +43,17 @@ class UsersController < ApplicationController
                                   :password_confirmation)
    end
 
+   def logged_in_user
+     unless logged_in?
+       store_location
+       flash[:danger] = "Please log in."
+       redirect_to login_url
+     end
+   end
+
+   # 正しいユーザーかどうか確認
+    def correct_user
+      @user = User.find(params[:id])
+       redirect_to(root_url) unless current_user?(@user)
+    end
 end
