d54fe54598646dc68339b3104ce0148e787e1d1e
taiga-ishii <tiger1410111@gmail.com>
2018-08-19 11:39:13 +0900

    remember-me

--- sample/sample_app/app/controllers/sessions_controller.rb
+++ sample/sample_app/app/controllers/sessions_controller.rb
@@ -5,12 +5,10 @@ class SessionsController < ApplicationController
  def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      # ユーザーログイン後にユーザー情報のページにリダイレクトする
       log_in user
-      remember user
+      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
       redirect_to user
     else
-      # エラーメッセージを作成する
       flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
     end
